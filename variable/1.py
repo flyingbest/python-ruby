34d9a9e6d7383656972acac6dd17665ff98cad3e
flyingbest <taeyoon4715@gmail.com>
2016-02-01 17:12:17 +0900

    variables

---
+++ variable/1.py
@@ -0,0 +1,9 @@
+print(10+5)
+x = 10
+y = 4
+print(x+y)
+
+title = "python & ruby"
+print("Title is " + title)
+
+
---
+++ variable/1.rb
@@ -0,0 +1,6 @@
+puts(10+5)
+x = 10
+puts(x+4)
+
+title = "python & ruby"
+puts("Title is " + title)
