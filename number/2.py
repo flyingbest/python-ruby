278a6780cb6d8833aebc572f0025b32e712cdfb1
flyingbest <taeyoon4715@gmail.com>
2016-02-01 14:13:33 +0900

    number and comments

---
+++ number/2.py
@@ -0,0 +1,10 @@
+import math
+
+print(math.ceil(2.2))	# 올림
+print(math.floor(2.7))	# 내림
+print(math.pow(2, 10))	# 제곱근
+print(math.pi)		# 파이값
+
+'''
+주석comment
+'''
---
+++ number/2.rb
@@ -0,0 +1,8 @@
+puts(2.2.ceil())
+puts(2.2.floor())
+puts(2**10)		# * 두개를 쓰면 제곱
+puts(Math::PI)		# 파이
+
+=begin
+주석 comment
+=end
