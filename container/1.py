14ea07861b94c60a81a37549635e48f7896a5927
flyingbest <taeyoon4715@gmail.com>
2016-02-15 15:23:38 +0900

    container simple test

---
+++ container/1.py
@@ -0,0 +1,16 @@
+print(type('taeyoon'))
+print(type(11))
+name = 'taeyoon'
+print(name)
+
+print(type(['taeyoon', 'someone', 'post']))
+
+mylists = ['taeyoon', 'someone', 'post']
+print(mylists)
+print(mylists[2])
+print(mylists[1])
+
+taeyoon = ['programmer', 'seoul', 27, False]
+taeyoon[1] = 'goyang'
+print(taeyoon)
+
---
+++ container/1.rb
@@ -0,0 +1,10 @@
+puts("taeyoon")
+name = 'taeyoon1'
+puts(name)
+
+names = ['taeyoon', 'someone', 'post']
+puts(names.class)
+puts(names[0])
+puts(names)
+name[0] = 'ansxodbs'
+print(names)	# 한줄로 출력하려면 print 로 해야됨.
